5d10a9ed79a13fddebc1d2b7a6a89131f9725975
Dom Lepizmo <dom.lepizmo@email.cz>
2017-12-04 18:24:21 +0100

    Add files via upload

--- author.sql
+++ author.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 27, 2017 at 06:23 PM
+-- Generation Time: Dec 04, 2017 at 05:21 PM
 -- Server version: 5.7.11
 -- PHP Version: 7.0.3
 
@@ -28,16 +28,55 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `author` (
   `id` int(40) NOT NULL,
-  `name` varchar(40) COLLATE utf8_czech_ci NOT NULL
+  `name` varchar(40) COLLATE utf8_czech_ci NOT NULL,
+  `real_name` varchar(40) COLLATE utf8_czech_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 --
 -- Dumping data for table `author`
 --
 
-INSERT INTO `author` (`id`, `name`) VALUES
-(1, 'tfk'),
-(2, 'tdg');
+INSERT INTO `author` (`id`, `name`, `real_name`) VALUES
+(1, 'tfk', 'Thousand foot krutch'),
+(2, 'tdg', 'Three days Grace'),
+(3, 'disturbed', 'Disturbed'),
+(4, 'apocalyptica', 'Apocalyptica');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `songs`
+--
+
+CREATE TABLE `songs` (
+  `id_song` int(20) NOT NULL,
+  `name` varchar(20) COLLATE utf8_czech_ci NOT NULL,
+  `real_name` varchar(40) COLLATE utf8_czech_ci NOT NULL,
+  `author` varchar(20) COLLATE utf8_czech_ci NOT NULL,
+  `real_author` varchar(40) COLLATE utf8_czech_ci NOT NULL,
+  `id_author` int(44) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Dumping data for table `songs`
+--
+
+INSERT INTO `songs` (`id_song`, `name`, `real_name`, `author`, `real_author`, `id_author`) VALUES
+(1, 'absolute', 'Absolute', 'tfk', 'Thousand foot krutch', 1),
+(2, 'riot', 'Riot', 'tdg', 'Three days Grace', 2),
+(3, 'hand_granade', 'Hand Granade', 'tfk', 'Thousand foot krutch', 1),
+(4, 'warrior', 'Warrior', 'disturbed', 'Disturbed', 3),
+(5, 'im_not_jesus', 'I\'m not Jesus', 'apocalyptica', 'Apocalyptica', 4);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `songs`
+--
+ALTER TABLE `songs`
+  ADD PRIMARY KEY (`id_song`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
