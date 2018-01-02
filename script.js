c11fe74c77121c4c23694a1ee2f20f1dce2bbe69
Dom Lepizmo <dom.lepizmo@email.cz>
2018-01-03 00:23:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,36 @@
-$(document).ready(function () {
-  $.ajax({
-    type: 'GET',
-    url: 'http://localhost/apicko/api',
-    data: 'value',
-    dataType: 'json',
-    success: function (data) {
-      $.each(data, function(i, author) {
-        $('body').append($('<div>', {
-          text: author.name
-        }));
-      });
-    }
-  });
+var urlAuthor = "http://localhost/apicko/api.php?action=get_author";
+var urlAuthorSongs = "http://localhost/apicko/api.php?action=get_author_songs&author=";
+var urlSongInfo = "http://localhost/apicko/api.php?action=get_song_info&song=";
+var urlGetAllSongs = "http://localhost/apicko/api.php?action=get_all_songs";
+
+$(document).ready(function(){
+  displayAuthorSongs();
+  displayAuthor()
 });
 
-
-
-function getJsonAuthor()
-{
-  $.getJSON("http://localhost/apicko/api.php?action=get_author", displayAuthor);
-};
-
-function getJsonSong()
-{
-  $.getJSON("http://localhost/apicko/api.php?action=get_author_songs", displayAllSongs);
-};
-
-function displayAuthor(data)
-{
-    $(data).each(function (i, author)
-    {
-        var a = document.createElement("a");
-        a.setAttribute("href", "#");
-        a.setAttribute("real_name", autor.name);
-
-        a.setAttribute("span", autor.real_name);
-        a.innerHTML = "<span>" + autor.real_name + "</span>";
-
+function displayAuthor(){
+  $( "#neco" ).click(function( event ){
+    event.preventDefault();
+    $.getJSON(urlAuthor,function(json){
+      $( "#neco" ).hide( "slow" );
+      $.each(json, function(i, item){
+        $( "#display" ).append('<a href="#" class="authorClass" id="' + json[i].name + '">' + json[i].real_name + '</a>');
+        $( "#display" ).append( "<br>" );
+      });
     });
-
-
-    $("a").click(function (a) {
-        console.log(a.target.real_name);
-        $.getJSON("http://localhost/apicko/api.php?action=get_author_songs", { "author": a.target.real_name }, displayAllSongs)
-    });
-
-
+  });
 }
-function displayAllSongs(data)
-{
-  $(data).each(function(i, song)
-  {
-    var a = document.createElement("a");
-    a.setAttribute("href","#");
-    a.setAttribute("real_name",song.name);
-    a.setAttribute("span", song.real_name);
-    a.innerHTML = "<span>" + song.real_name + "</span>";
 
+function displayAuthorSongs(){
+  $( "#tfk" ).click(function( event ){
+    event.preventDefault();
+    $( "#tfk" ).hide( "slow" );
+    urlAuthorSong += $(this).attr( "id" );
+    $.getJSON(urlAuthorSong,function(json){
+      $.each(json, function(i, item){
+        $( "#display" ).append('<a href="#">' + json[i].real_name + '</a>');
+        $( "#display" ).append( "<br>" );
+      });
+    });
   });
-
-  $("a").click(function (a) {
-      console.log(a.target.real_name);
-      $.getJSON("http://localhost/apicko/api.php?action=get_author_songs", { "author": a.target.real_name }, displayAllSongs)
-  });
+}
